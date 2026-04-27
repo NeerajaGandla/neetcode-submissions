b4398c0180bd6bd8dd0173e0ff17c7272b7aeb31
Neeraja <gaandlaneeraja@gmail.com>
2026-04-27 07:36:22 +0530

    Add: same-binary-tree - submission-0

---
+++ Data Structures & Algorithms/same-binary-tree/submission-0.kt
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        if(p?.`val` != q?.`val`) return false
+        if(p == null && q == null) return true
+        return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+    }
+}
+
